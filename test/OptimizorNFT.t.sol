b0e9a8df8058a7d4dffd265a7fa8cd55541ae81e
Leo Alt <leo@ethereum.org>
2022-06-13 10:12:58 +0200

    sqrt challenge

--- test/OptimizorNFT.t.sol
+++ test/OptimizorNFT.t.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "./ChallengeIDs.sol";
 
 import "../src/OptimizorNFT.sol";
 import "../src/SumChallenge.sol";
+import "../src/SqrtChallenge.sol";
 
-uint constant SUM_ID = 0;
-uint constant NON_USED_ID = type(uint).max;
+import "forge-std/Test.sol";
 
 contract OptimizorTest is Test {
 	Optimizor opt;
 	Challenge sum = new SumChallenge();
+	Challenge sqrt = new SqrtChallenge();
 
     function setUp() public {
 		opt = new Optimizor();
@@ -23,6 +24,10 @@ contract OptimizorTest is Test {
 
 	function addSumChallenge() internal {
 		opt.addChallenge(SUM_ID, sum);
+	}
+
+	function addSqrtChallenge() internal {
+		opt.addChallenge(SQRT_ID, sqrt);
 	}
 
 	function testDupAddSumChallenge() public {
@@ -43,6 +48,18 @@ contract OptimizorTest is Test {
 	function testNonExistentChallenge() public {
 		vm.expectRevert(abi.encodeWithSignature("NoChallenge(uint256)", type(uint).max));
 		opt.optimize(NON_USED_ID, address(0));
+    }
+
+	function testCheapSqrt() public {
+		addSqrtChallenge();
+		runCheapSqrt();
+	}
+
+	function runCheapSqrt() internal {
+		CheapSqrt ch = new CheapSqrt();
+		opt.optimize(SQRT_ID, address(ch));
+		(,, address o) = opt.challenges(SQRT_ID);
+		assertEq(o, address(ch));
     }
 
 	function testCheapSum() public {
@@ -74,6 +91,28 @@ contract OptimizorTest is Test {
 		runExpensiveSum();
 		runCheapSum();
     }
+}
+
+contract CheapSqrt is ISqrt {
+	function sqrt(uint64[INPUT_SIZE] calldata inputs) external pure returns (uint64[INPUT_SIZE] memory outputs) {
+		for (uint i = 0; i < inputs.length; ++i)
+			outputs[i] = sqrt_one(inputs[i]);
+	}
+
+	function sqrt_one(uint64 input) internal pure returns (uint64 output) {
+		uint l = 0;
+		uint r = input - 1;
+		while (l < r) {
+			uint m = (l + r) / 2;
+			if ((m * m) <= input && ((m + 1) * (m + 1)) > input)
+				return uint64(m);
+			if (m * m < input)
+				l = m;
+			else
+				r = m;
+		}
+		revert("wrong algorithm");
+	}
 }
 
 contract CheapSum is ISum {
