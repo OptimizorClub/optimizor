f11982f36c9d2a5d7c0cf23a18fbe7f5b41e995f
Leo Alt <leo@ethereum.org>
2022-06-15 17:53:11 +0200

    forgot new file

---
+++ test/SumChallenge.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "../src/Challenge.sol";
+
+interface ISum {
+	error WrongSum(uint, uint, uint);
+
+	function sum(uint, uint) external view returns (uint);
+}
+
+contract SumChallenge is IChallenge {
+	function run(address opzor, uint seed) external view override returns (uint) {
+		// Generate input.
+		(uint x, uint y) = (random(seed), random(++seed));
+
+		uint preGas = gasleft();
+		uint s = ISum(opzor).sum(x, y);
+		uint postGas = gasleft();
+
+		verify(x, y, s);
+
+		unchecked { return preGas - postGas; }
+	}
+
+	function verify(uint x, uint y, uint s) internal pure {
+		unchecked {
+			if (s != (x + y))
+				revert ISum.WrongSum(x, y, s);
+		}
+	}
+
+	function random(uint seed) internal view returns (uint) {
+		return uint(keccak256(abi.encodePacked(block.timestamp, seed))) % 100;
+	}
+}
