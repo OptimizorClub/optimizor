3b61a51d033345d67e48c34e18e6baf1f0377937
Leo Alt <leo@ethereum.org>
2022-06-16 13:43:02 +0200

    organize tests

--- test/OptimizorNFT.t.sol
+++ test/OptimizorNFT.t.sol
@@ -1,97 +1,50 @@
 // SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.14;
 
-import "./SumChallenge.sol";
+import "./BaseTest.sol";
 import "../src/OptimizorNFT.sol";
 import "forge-std/Test.sol";
 
-uint constant SUM_ID = 0;
-uint constant NON_USED_ID = type(uint).max;
-
-contract OptimizorTest is Test {
-	Optimizor opt;
-	IChallenge sum = new SumChallenge();
-
-    function setUp() public {
-		opt = new Optimizor();
-	}
-
-	function testAddSumChallenge() public {
-		addSumChallenge();
-	}
-
-	function addSumChallenge() internal {
-		opt.addChallenge(SUM_ID, sum);
-	}
-
-	function testDupAddSumChallenge() public {
-		addSumChallenge();
-
-		vm.expectRevert(abi.encodeWithSignature("ChallengeExists(uint256)", uint(0)));
-		addSumChallenge();
-	}
-
-	function testAddChallengeNonAdmin() public {
-		// TODO
-	}
-
-	function testAddExistingChallengeNonAdmin() public {
-		// TODO
-	}
-
-	function testNonExistentChallenge() public {
-		vm.roll(block.number + 512);
-		vm.expectRevert(abi.encodeWithSignature("ChallengeNotFound(uint256)", type(uint).max));
-		opt.challenge(NON_USED_ID, bytes32(0), address(0), address(0));
-    }
-
+contract OptimizorTest is BaseTest {
 	function testCheapSum() public {
 		addSumChallenge();
-		runCheapSum();
-	}
 
-	function runCheapSum() internal {
-		CheapSum ch = new CheapSum();
-		opt.commit(address(ch).codehash);
+		opt.commit(address(cheapSum).codehash);
+		advancePeriod();
+		advancePeriod();
+		opt.challenge(SUM_ID, address(cheapSum).codehash, address(cheapSum), address(this));
 
-		vm.roll(block.number + 512);
-
-		opt.challenge(SUM_ID, address(ch).codehash, address(ch), address(this));
 		(,, address o,) = opt.challenges(SUM_ID);
 		assertEq(o, address(this));
-    }
-
-	/*
-	function testExpensiveSum() public {
-		addSumChallenge();
-		runExpensiveSum();
 	}
 
-	function runExpensiveSum() internal {
-		ExpensiveSum exp = new ExpensiveSum();
-		opt.challenge(SUM_ID, address(exp));
-		(,, address o) = opt.challenges(SUM_ID);
-		assertEq(o, address(exp));
-    }
+	function testExpensiveSum() public {
+		addSumChallenge();
+
+		opt.commit(address(expSum).codehash);
+		advancePeriod();
+		advancePeriod();
+		opt.challenge(SUM_ID, address(expSum).codehash, address(expSum), address(this));
+
+		(,, address o,) = opt.challenges(SUM_ID);
+		assertEq(o, address(this));
+	}
 
     function testCheapExpensiveSum() public {
 		addSumChallenge();
-		runExpensiveSum();
-		runCheapSum();
+
+		opt.commit(address(cheapSum).codehash);
+		opt.commit(address(expSum).codehash);
+
+		advancePeriod();
+		advancePeriod();
+
+		opt.challenge(SUM_ID, address(expSum).codehash, address(expSum), address(this));
+		(,, address o1,) = opt.challenges(SUM_ID);
+		assertEq(o1, address(this));
+
+		opt.challenge(SUM_ID, address(cheapSum).codehash, address(cheapSum), address(this));
+		(,, address o2,) = opt.challenges(SUM_ID);
+		assertEq(o2, address(this));
     }
-	*/
-}
-
-contract CheapSum is ISum {
-	function sum(uint x, uint y) external pure returns (uint) {
-		return x + y;
-	}
-}
-
-contract ExpensiveSum is ISum {
-	function sum(uint x, uint y) external pure returns (uint) {
-		for (uint i = 0; i < y; ++i)
-			++x;
-		return x;
-	}
 }
