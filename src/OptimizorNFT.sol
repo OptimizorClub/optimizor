869d02791fa20bc123192376e9c1afc605e98bb4
Leo Alt <leo@ethereum.org>
2022-06-17 16:31:15 +0200

    example svg that works on OS

--- src/OptimizorNFT.sol
+++ src/OptimizorNFT.sol
@@ -104,11 +104,16 @@ contract Optimizor is Owned, ReentrancyGuard, Time, ERC721 {
 		return uint32(id);
 	}
 
+	function svg() internal pure returns (string memory) {
+		return "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'>\
+  <rect width='300' height='100' style='fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)' />\
+</svg>";
+	}
+
     function tokenURI(uint256 /*id*/) public pure override returns (string memory) {
 		//uint32 level = uint32(id);
 		//bool winner = winnerLevel(id) == level;
 
-		//string memory image_data = "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' height='100' width='100' style='background-color:green'%3E%3Ccircle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill='red' /%3E%3C/svg%3E";
 		string memory meta = string(
 			abi.encodePacked(
 				'{\n"name": "', "TestName",
@@ -117,19 +122,10 @@ contract Optimizor is Owned, ReentrancyGuard, Time, ERC721 {
 			)
 		);
 
-		/*
 		meta = string(
 			abi.encodePacked(
 				meta,
-				'"\n,"image_data": "', image_data
-			)
-		);
-		*/
-
-		meta = string(
-			abi.encodePacked(
-				meta,
-				'\n,"image": "', "https://freesvg.org/img/Placeholder.png"
+				'\n,"image": "data:image/svg+xml;base64,', Base64.encode(bytes(svg()))
 			)
 		);
 
