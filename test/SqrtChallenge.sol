ee59c56299de95ff0377638c5bda1fc25755ed13
Leo Alt <leo@ethereum.org>
2022-06-24 15:48:25 +0200

    sqrt stuff works

--- test/SqrtChallenge.sol
+++ test/SqrtChallenge.sol
@@ -12,7 +12,7 @@ uint constant INPUT_SIZE = 3;
 Fixed18 constant EPSILON = Fixed18.wrap(0.0001 * 10**18);
 
 interface ISqrt {
-	function sqrt(Fixed18[INPUT_SIZE] calldata) external view returns (Fixed18[INPUT_SIZE] memory);
+    function sqrt(Fixed18[INPUT_SIZE] calldata) external view returns (Fixed18[INPUT_SIZE] memory);
 }
 
 function random_fixed18(uint256 seed) view returns (Fixed18) {
@@ -27,36 +27,36 @@ contract SqrtChallenge is IChallenge {
 
     error DoesNotSatisfyTolerance(uint input, uint output);
 
-	function run(address target, uint seed) external view override returns (uint) {
-		// Generate inputs.
-	    Fixed18[INPUT_SIZE] memory inputs;
-		unchecked {
-			for (uint i = 0; i < INPUT_SIZE; ++i) {
-				inputs[i] = random_fixed18(seed);
+    function run(address target, uint seed) external view override returns (uint) {
+        // Generate inputs.
+        Fixed18[INPUT_SIZE] memory inputs;
+        unchecked {
+            for (uint i = 0; i < INPUT_SIZE; ++i) {
+                inputs[i] = random_fixed18(seed);
                 // TODO should the inputs be uniform?
                 // TODO Maybe use a Linear Congruential Generator?
-				seed = Fixed18.unwrap(inputs[i]);
-			}
-		}
+                seed = Fixed18.unwrap(inputs[i]);
+            }
+        }
 
-		uint preGas = gasleft();
-		Fixed18[INPUT_SIZE] memory outputs = ISqrt(target).sqrt(inputs);
-		uint usedGas;
-		unchecked { usedGas = preGas - gasleft(); }
+        uint preGas = gasleft();
+        Fixed18[INPUT_SIZE] memory outputs = ISqrt(target).sqrt(inputs);
+        uint usedGas;
+        unchecked { usedGas = preGas - gasleft(); }
 
         verify(inputs, outputs);
 
-		return usedGas;
-	}
+        return usedGas;
+    }
 
     // Reverts if invalid
-	function verify(Fixed18[INPUT_SIZE] memory inputs, Fixed18[INPUT_SIZE] memory outputs) internal pure {
-		unchecked {
-			for (uint i = 0; i < INPUT_SIZE; ++i) {
-				verify(inputs[i], outputs[i]);
-			}
-		}
-	}
+    function verify(Fixed18[INPUT_SIZE] memory inputs, Fixed18[INPUT_SIZE] memory outputs) internal pure {
+        unchecked {
+            for (uint i = 0; i < INPUT_SIZE; ++i) {
+                verify(inputs[i], outputs[i]);
+            }
+        }
+    }
 
     // Reverts if invalid
     function verify(Fixed18 input, Fixed18 output) internal pure {
@@ -65,14 +65,14 @@ contract SqrtChallenge is IChallenge {
         //       --------------------------  < EPSILON
         //       |        output           |
         if(
-           !output
-           .mul(output)
-           .distance(input)
-           .div(output)
-           .lt(EPSILON)
-        )
+            !output
+            .mul(output)
+            .distance(input)
+            .div(output)
+            .lt(EPSILON)
+        ) {
             revert DoesNotSatisfyTolerance(Fixed18.unwrap(input), Fixed18.unwrap(output));
-
+        }
     }
 
     function name() external override view returns (string memory) {
@@ -117,55 +117,58 @@ contract SqrtChallenge is IChallenge {
 /// Returns the midpoint avoiding phantom overflow
 function mid(uint a, uint b) pure returns (uint) {
     unchecked {
-		return (a & b) + (a ^ b) / 2;
+        return (a & b) + (a ^ b) / 2;
     }
 }
 
-contract CheapSqrt {
-    function sqrt(uint[INPUT_SIZE] calldata inputs) external returns (uint[INPUT_SIZE] memory outputs) {
-		for (uint i = 0; i < inputs.length; ++i)
-			outputs[i] = sqrt_one(inputs[i]);
-	}
+contract CheapSqrt is ISqrt {
+    function sqrt(Fixed18[INPUT_SIZE] calldata inputs) external pure returns (Fixed18[INPUT_SIZE] memory outputs) {
+        for (uint i = 0; i < inputs.length; ++i) {
+            outputs[i] = sqrt_one(inputs[i]);
+        }
+    }
 
-    function sqrt_one(uint input) internal returns (uint output) {
-		uint l = 0;
-		uint r = input - 1;
-		while (l < r) {
+    function sqrt_one(Fixed18 input) internal pure returns (Fixed18 output) {
+        uint l = 0;
+        uint uInput = Fixed18.unwrap(input);
+        uint r = uInput - 1;
+        while (l < r) {
             uint m = mid(l, r);
-			uint mPlus1 = m + 1;
-            if ((m * m <= input) && (mPlus1 * mPlus1 > input))
-				return m * 10**9;
-            if (m * m < input)
-				l = m;
-			else
-				r = m;
-		}
-		revert("wrong algorithm");
-	}
+            uint mPlus1 = m + 1;
+            if ((m * m <= uInput) && (mPlus1 * mPlus1 > uInput))
+                return Fixed18.wrap(m * 10**9);
+            if (m * m < uInput)
+                l = m;
+            else
+                r = m;
+        }
+        revert("wrong algorithm");
+    }
 }
 
-contract ExpensiveSqrt {
-    function sqrt(uint[INPUT_SIZE] calldata inputs) external returns (uint[INPUT_SIZE] memory outputs) {
-		for (uint i = 0; i < inputs.length; ++i) {
-			outputs[i] = sqrt_one(inputs[i]);
-			outputs[i] = sqrt_one(inputs[i]);
-			outputs[i] = sqrt_one(inputs[i]);
+contract ExpensiveSqrt is ISqrt {
+    function sqrt(Fixed18[INPUT_SIZE] calldata inputs) external pure returns (Fixed18[INPUT_SIZE] memory outputs) {
+        for (uint i = 0; i < inputs.length; ++i) {
+            outputs[i] = sqrt_one(inputs[i]);
+            outputs[i] = sqrt_one(inputs[i]);
+            outputs[i] = sqrt_one(inputs[i]);
         }
-	}
+    }
 
-    function sqrt_one(uint input) internal returns (uint output) {
-		uint l = 0;
-		uint r = input - 1;
-		while (l < r) {
+    function sqrt_one(Fixed18 input) internal pure returns (Fixed18 output) {
+        uint l = 0;
+        uint uInput = Fixed18.unwrap(input);
+        uint r = uInput - 1;
+        while (l < r) {
             uint m = mid(l, r);
-			uint mPlus1 = m + 1;
-            if ((m * m <= input) && (mPlus1 * mPlus1 > input))
-				return m * 10**9;
-            if (m * m < input)
-				l = m;
-			else
-				r = m;
-		}
-		revert("wrong algorithm");
-	}
+            uint mPlus1 = m + 1;
+            if ((m * m <= uInput) && (mPlus1 * mPlus1 > uInput))
+                return Fixed18.wrap(m * 10**9);
+            if (m * m < uInput)
+                l = m;
+            else
+                r = m;
+        }
+        revert("wrong algorithm");
+    }
 }
