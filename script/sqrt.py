584ea5c546a24fff743c7c50f94946e11644dbec
hrkrshnn <webmail.hari@gmail.com>
2022-06-24 16:25:50 +0200

    Added a rotating animation for the polygon

--- script/sqrt.py
+++ script/sqrt.py
@@ -1,21 +1,22 @@
 height = 230
 N = 13
-base_length = height / 6
+base_length = height / 8
 
 def next_vertex(x, y):
     h = (x**2 + y**2)**0.5
     return (x - y/h, y + x/h)
 
 # Code for the sqrt svg
-out = f'<svg height="{height}" width="{height}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n'
-out += f'<rect width="{height}" height="{height}" rx="18px" ry="18px" fill="rgba(0,0,0,0.1)" />'
+prelude = f'<svg height="{height}" width="{height}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n'
+out = f'<rect width="{height}" height="{height}" rx="18px" ry="18px" fill="rgba(0,0,0,0.1)" />'
+print(f"    if (level == 0)\n        return '{out}';")
 
 # Origin
 o_x, o_y = 0, 0
 b_x, b_y = 1, 0
 
 # Center
-c_x, c_y = (height / 2), (height / 2) + 30
+c_x, c_y = (height / 2), (height / 2)
 
 for i in range(0, N):
     n_x, n_y = next_vertex(b_x, b_y)
@@ -25,13 +26,17 @@ for i in range(0, N):
     p2_y = c_y + base_length * b_y
     p3_x = c_x + base_length * n_x
     p3_y = c_y + base_length * n_y
-    out += f'<polygon points="{round(p1_x)},{round(p1_y)} {round(p2_x)},{round(p2_y)} {round(p3_x)},{round(p3_y)}" fill="none" stroke="white"/>'
+    out += f'<polygon points="{round(p1_x)},{round(p1_y)} {round(p2_x)},{round(p2_y)} {round(p3_x)},{round(p3_y)}" fill="none" stroke="white" stroke-width="2">'
+    out += f'<animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 {round(p1_x)} {round(p1_y)}" to="360 {round(p1_x)} {round(p1_y)}" dur="10s" repeatCount="indefinite"/></polygon>'
+
     b_x, b_y = n_x, n_y
+    if i != N - 1:
+        print(f"    if (level == {i + 1})\n        return '{out}';")
+    else:
+        print(f"    else\n        return '{out}';")
 
-# out += '<polygon points="100,10 200,190 100,210" />'
-out += '</svg>\n'
+svg = prelude + out + '\n</svg>\n'
 
-f = open("sqrt.svg", 'w')
-f.write(out)
-f.close()
+with open("sqrt.svg", 'w') as f:
+    f.write(svg)
 
