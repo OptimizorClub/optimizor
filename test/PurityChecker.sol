8705566703477a9de0671fff803c297a62b721c5
Alex Beregszaszi <alex@rtfs.hu>
2022-07-01 12:47:48 +0200

    Add purity checker

---
+++ test/PurityChecker.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "../src/IPurityChecker.sol";
+
+import "puretea/Puretea.sol";
+
+contract PurityChecker is IPurityChecker {
+    uint256 constant acceptedOpcodesMask = 0x600800000000000000000000ffffffffffffffff0fdf01ff67ff00013fff0fff;
+
+    function check(address account) external view returns (bool) {
+        return Puretea.check(account.code, acceptedOpcodesMask);
+    }
+}
