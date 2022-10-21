10f9d98543cf6387e67f2c3cf34a2c2358bad32f
Alex Beregszaszi <alex@rtfs.hu>
2022-10-22 00:20:57 +0200

    Change PurityTest to cover PurityChecker

--- test/Purity.t.sol
+++ test/Purity.t.sol
@@ -3,20 +3,28 @@ pragma solidity ^0.8.15;
 
 import "./BaseTest.sol";
 
-import "../src/PurityChecker.sol";
-import "./SolidityMetadata.sol";
-
-import "puretea/Puretea.sol";
-
 contract PurityTest is BaseTest {
-    uint256 constant acceptedOpcodesMask = 0x600800000000000000000000ffffffffffffffff0fdf01ff67ff00013fff0fff;
+    function testCheapSumPurity() public {
+        assertTrue(
+            purity.check(address(cheapSum))
+        );
+    }
+
+    function testExpSumPurity() public {
+        assertTrue(
+            purity.check(address(expSum))
+        );
+    }
 
     function testCheapSqrtPurity() public {
         assertTrue(
-            Puretea.check(
-                address(cheapSqrt).code,
-                acceptedOpcodesMask
-            )
+            purity.check(address(cheapSqrt))
+        );
+    }
+
+    function testExpSqrtPurity() public {
+        assertTrue(
+            purity.check(address(expSqrt))
         );
     }
 }
