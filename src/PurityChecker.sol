df887100853afe309378e90fef1197323cfcb44a
Alex Beregszaszi <alex@rtfs.hu>
2022-10-26 18:23:45 +0200

    Document PurityChecker

--- src/PurityChecker.sol
+++ src/PurityChecker.sol
@@ -6,6 +6,7 @@ import {IPurityChecker} from "src/IPurityChecker.sol";
 import {Puretea} from "puretea/Puretea.sol";
 
 contract PurityChecker is IPurityChecker {
+    // Allow non-state modifying opcodes only.
     uint256 private constant acceptedOpcodesMask = 0x600800000000000000000000ffffffffffffffff0fdf01ff67ff00013fff0fff;
 
     function check(address account) external view returns (bool) {
