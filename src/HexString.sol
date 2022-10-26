ea86166c73f878cbfad8b529220d3ae345838140
Alex Beregszaszi <alex@rtfs.hu>
2022-10-27 00:51:13 +0200

    Make HexString unchecked

--- src/HexString.sol
+++ src/HexString.sol
@@ -9,9 +9,11 @@ library HexString {
 
     function toHexStringNoPrefix(uint256 value, uint256 length) internal pure returns (string memory) {
         bytes memory buffer = new bytes(2 * length);
-        for (uint256 i = buffer.length; i > 0; i--) {
-            buffer[i - 1] = ALPHABET[value & 0xf];
-            value >>= 4;
+        unchecked {
+            for (uint256 i = buffer.length; i > 0; --i) {
+                buffer[i - 1] = ALPHABET[value & 0xf];
+                value >>= 4;
+            }
         }
         if (value != 0) revert HexLengthInsufficient();
         return string(buffer);
@@ -21,9 +23,11 @@ library HexString {
         bytes memory buffer = new bytes(2 * length + 2);
         buffer[0] = '0';
         buffer[1] = 'x';
-        for (uint256 i = 2 * length + 1; i > 1; --i) {
-            buffer[i] = ALPHABET[value & 0xf];
-            value >>= 4;
+        unchecked {
+            for (uint256 i = 2 * length + 1; i > 1; --i) {
+                buffer[i] = ALPHABET[value & 0xf];
+                value >>= 4;
+            }
         }
         if (value != 0) revert HexLengthInsufficient();
         return string(buffer);
