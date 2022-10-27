49af5d78b9ce3600f484de823392d81683ebba21
Leo Alt <leo@ethereum.org>
2022-10-27 23:45:47 +0200

    fmt

--- src/NFTSVG.sol
+++ src/NFTSVG.sol
@@ -167,7 +167,11 @@ library NFTSVG {
         );
     }
 
-    function generateSVGCardMantle(string memory challengeName, uint32 rank, uint32 participants) private pure returns (string memory svg) {
+    function generateSVGCardMantle(string memory challengeName, uint32 rank, uint32 participants)
+        private
+        pure
+        returns (string memory svg)
+    {
         svg = string.concat(
             '<g mask="url(#fade-symbol)"><rect fill="none" x="0px" y="0px" width="290px" height="200px"/><text y="70px" x="32px" fill="#fff" font-family="\'Courier New\', monospace" font-weight="200" font-size="36px">',
             challengeName,
