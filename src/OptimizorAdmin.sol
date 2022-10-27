40bd4ef14fde6570d79720f428bdadc2b947204a
Leo Alt <leo@ethereum.org>
2022-10-27 12:40:26 +0200

    add test for purity checker update event

--- src/OptimizorAdmin.sol
+++ src/OptimizorAdmin.sol
@@ -9,19 +9,22 @@ import {IChallenge} from "src/IChallenge.sol";
 import {Owned} from "solmate/auth/Owned.sol";
 
 contract OptimizorAdmin is OptimizorNFT, Owned {
-    IPurityChecker purity;
+    IPurityChecker public purity;
 
     error ChallengeExists(uint256 challengeId);
 
+    event PurityCheckerUpdated(IPurityChecker newPurityChecker);
     event ChallengeAdded(uint256 challengeId, IChallenge);
 
     constructor(IPurityChecker _purity) Owned(msg.sender) {
-        purity = _purity;
+        updatePurityChecker(_purity);
     }
 
     /// @dev Purity checker may need to be updated when there are EVM changes.
-    function updatePurityChecker(IPurityChecker _purity) external onlyOwner {
+    function updatePurityChecker(IPurityChecker _purity) public onlyOwner {
         purity = _purity;
+
+        emit PurityCheckerUpdated(_purity);
     }
 
     function addAttribute(IAttribute attr) external onlyOwner {
---
+++ test/PurityCheckerManagement.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {BaseTest} from "test/BaseTest.sol";
+import {PurityChecker} from "src/PurityChecker.sol";
+
+contract PurityCheckerManagementTest is BaseTest {
+    // We need to redefine the event here because the compiler won't
+    // let us reuse the one from OptimizorAdmin.
+    event PurityCheckerUpdated(PurityChecker newPurityChecker);
+
+    function testNewPurityChecker() public {
+        PurityChecker newChecker = new PurityChecker();
+
+        vm.expectEmit(true, false, false, false);
+        emit PurityCheckerUpdated(newChecker);
+
+        opt.updatePurityChecker(newChecker);
+
+        assertEq(address(newChecker), address(opt.purity()));
+    }
+}
